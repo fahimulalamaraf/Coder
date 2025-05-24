b5c7351176228b1effb97f83af7862c35d1189ca
fahimulalam <fahimul.alam@bjitgroup.com>
2025-05-24 16:20:15 +0600

    Added APIRouter architecture

--- Phase 01/app/main.py
+++ Phase 01/app/main.py
@@ -4,15 +4,15 @@ from fastapi import FastAPI, status, HTTPException, Depends
 import uvicorn
 from sqlalchemy.orm import defer
 
+from app.todos.routes import book_router
+
 app = FastAPI()
 
-
-
+app.include_router(book_router, prefix="/books", tags=["books"])
 
 def main():
     print("Hello from phase-01!")
     uvicorn.run(app, host="localhost", port=8000)
-
 
 
 if __name__ == "__main__":
